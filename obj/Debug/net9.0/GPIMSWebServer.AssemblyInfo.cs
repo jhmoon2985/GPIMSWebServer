49e988719536dfcf8680f9bd0af9f861688f00ff
jhmoon2985 <jhmoon2985@gmail.com>
2025-06-10 17:00:16 +0900

    화면추가

--- obj/Debug/net9.0/GPIMSWebServer.AssemblyInfo.cs
+++ obj/Debug/net9.0/GPIMSWebServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GPIMSWebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6be78b80586373120f0e3baf2803df75e6aa55f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GPIMSWebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GPIMSWebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
